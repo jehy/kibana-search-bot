01a82aa2ff3aa729004dcb7bf8a8672b7ed5de3e
Evgeniy Bondarenko <evgeniy.bondarenko@pay-me.ru>
2017-05-23 13:42:56 +0300

    remove extra dependency

--- process.js
+++ process.js
@@ -1,6 +1,5 @@
 const
   config    = require('./config/config.json'),
-  colors    = require('colors/safe'),
   rp        = require('request-promise'),
   Promise   = require('bluebird'),
   userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.29 Safari/537.36';
